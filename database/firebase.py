5af4e12571f09cbe7a7ffd83ea5d9a68786308c2
OdinEOJ <odiovr@kvn.no>
2025-01-17 10:59:53 +0100

    i

--- database/firebase.py
+++ database/firebase.py
@@ -2,13 +2,11 @@ import firebase_admin
 import uuid
 from firebase_admin import credentials, firestore
 
-# Path to your service account key
-cred = credentials.Certificate("database/mongodb-c9c44-firebase-adminsdk-t0fim-abd6cc14b3.json")
+cred = credentials.Certificate("database/mongodb-c9c44-firebase-adminsdk-t0fim-9f731cb975.json")
 
-# Initialize Firestore
 firebase_admin.initialize_app(cred)
 
-# Reference the Firestore database
+
 db = firestore.client()
 
 firstName = 'firstName'
@@ -18,23 +16,22 @@ def leggTilEnBruker():
     docID = str(uuid.uuid4())
     doc_ref = db.collection(collection).document(docID)
     doc_ref.set({
-        'firstName': input("Student first name: "),
-        'lastName': input("Student last name: "),
-        'age': int(input("Student age: ")),
-        'class': input("Student class: "),
+        'firstName': input("Fornavn: "),
+        'lastName': input("Etternavn: "),
+        'age': int(input("Alder: ")),
+        'class': input("Klasse: "),
     })
-    print(f"User added with ID: {docID}")
+    print(f"Bruker med ID: {docID} er lagt til!")
 
 
 def printBrukerInfo():
     collection = str(input("Hvilken collection er denne brukeren i?: "))
-    doc_id = input("Enter the document ID to view: ")
-    doc = db.collection(collection).document(doc_id).get()
-    if doc.exists:
-        print(f"Her er brukeren du lette etter: {doc.to_dict()}")
-    else:
-        print("Finner ingen bruker med denne id!")
-
+    first_name = input("Legg til dokument ID for å se bruker: ")
+    users = db.collection(collection).where('firstName', '==', first_name).stream()
+    for user in users:
+        print(user.id)
+        print(user.to_dict())
+    
 
 def slettEnBruker():
     collection = input("Hvilken collection er denne brukeren i?: ")
@@ -51,12 +48,51 @@ def slettEnBruker():
     if deleted_count == False:
         print(f"Finner ingen bruker med navnet '{first_name}'")
 
+def rediger():
+    collection = input("Hvilken collection er brukeren du vil endre i?: ")
+    first_name = input("Hvilken bruker vil du slette?: ")
+    
+    if collection == "Lærere":
+        doc_ref_Lærere = db.collection("Lærere").document(first_name)
+        firstName = input("fornavn: ")
+        lastName = input("etternavn: ")
+        age = input("alder: ")
+
+        fag = []
+        while True:
+            fag.append(input("Legg til fag: "))
+            flereFag = input("Ønsker du å legge til flere fag? ja / nei: ")
+            if(flereFag == "nei"):
+                break
+
+        doc_ref_Lærere.update({
+        "firstName": firstName,
+        "lastName": lastName,
+        "fag": fag,
+        "age": age
+        })
+
+    elif collection == "Elever":
+        doc_ref_Elever = db.collection("Elever").document(first_name)
+        firstName = input("fornavn: ")
+        lastName = input("etternavn: ")
+        klasse = input("klasse: ")
+        age = input("alder: ")
+    
+        doc_ref_Elever.update({
+            "firstName": firstName,
+            "lastName": lastName,
+            "klasse": klasse,
+            "age": age
+        })
+
 
 def meny():
     print("-----------Hovedmeny----------")
     print("   1. Legg til ny bruker")
     print("   2. Slett en bruker")
     print("   3. Se bruker")
+    print("   4. rediger bruker")
     print("   0. Avslutt")
     valg = input("Velg fra menyen: ")
     return valg
@@ -71,6 +107,8 @@ def main():
             slettEnBruker()
         elif valgt == "3":
             printBrukerInfo()
+        elif valgt == "4":
+            rediger()
         elif valgt == "0":
             run = False
         
